f6ef0ab475545b928cda78f29220288473e23033
mohemd98 <mohemd.web@gmail.com>
2023-12-04 19:24:08 +0300

    2023/12/4

---
+++ app/Models/Setting.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    use HasFactory;
+    protected $fillable = [ 'name', 'description', 'logo', 'favicon', 'email', 'phone', 'address', 'facebook', 'twitter', 'instagram', 'youtube', 'tiktok'];
+}
